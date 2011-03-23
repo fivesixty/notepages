15500fb42e35f05e822dcb033fb8ddeee55b9f32
c-spencer <cs560@cam.ac.uk>
2011-03-23 16:18:17 +0000

    Reorganise interface.js

--- assets/interface.js
+++ assets/interface.js
@@ -12,90 +12,45 @@ $(window).load(function () {
   size_images($("#output")[0]);
 });
 
+$.fn.quickdiff("filter", "mathSpanInline",
+  function (node) { return (node.nodeName === "SPAN"
+                            && $(node).hasClass("mathInline")); },
+  function (a, b) {
+    var aHTML = $.trim($("script", a).html()), bHTML = $.trim($(b).html());
+    return ("%%" + aHTML + "%%") !== bHTML;
+  } );
+
+$.fn.quickdiff("filter", "mathSpan",
+  function (node) { return (node.nodeName === "SPAN"
+                            && $(node).hasClass("math")); },
+  function (a, b) {
+    var aHTML = $.trim($("script", a).html()), bHTML = $.trim($(b).html());
+    return ("$$" + aHTML + "$$") !== bHTML;
+  } );
+
 $(document).ready(function () {
+  var loaded = editing,
+    previewing = false,
+    editor_scrolltop = 0,
+    editor_position = {start: 0, end: 0},
+    preview_scrolltop = 0,
+    markdown = new Showdown.converter(),
+    narrowscreen = false,
+    inputarea = $("textarea"),
+    preproc = $("<div></div>"),
+    outputel = $("#output > div"),
+    renderDelay = 50;
+    
+  reconstrain();
+  setEditing(editing);
+  
   var wHeight = $(window).height();
   var wWidth = $(window).width();
-  var loaded = editting;
-  if (editting) {
-    $("#options-inner").css({marginTop: 0});
-  } else {
-    $("#savebutton").attr("disabled", true);
-  }
-  var previewing = false;
-  editor_scrolltop = 0;
-  editor_position = {start: 0, end: 0};
-  preview_scrolltop = 0;
-  
-  markdown = new Showdown.converter();
-  
-  var narrowscreen = false;
-  
-  var inputarea = $("textarea");
-  var preproc = $("<div></div>");
-  var outputel = $("#output > div");
-  
-  $.fn.quickdiff("filter", "mathSpanInline",
-    function (node) { return (node.nodeName === "SPAN"
-                              && $(node).hasClass("mathInline")); },
-    function (a, b) {
-      var aHTML = $.trim($("script", a).html()), bHTML = $.trim($(b).html());
-      return ("%%" + aHTML + "%%") !== bHTML;
-    } );
-  
-  $.fn.quickdiff("filter", "mathSpan",
-    function (node) { return (node.nodeName === "SPAN"
-                              && $(node).hasClass("math")); },
-    function (a, b) {
-      var aHTML = $.trim($("script", a).html()), bHTML = $.trim($(b).html());
-      return ("$$" + aHTML + "$$") !== bHTML;
-    } );
-  
-  var lastrendertime = 0;
-  var renderDelay = 0;
-  var markdownTime = 0;
-  var diffTime = 0;
-  
-  function setRenderDelay() {
-    if (lastrendertime > 200) {
-      renderDelay = 400;
-    } else if (lastrendertime > 10) {
-      renderDelay = 50;
-    }
-    $("#delay").html((lastrendertime) + "ms : " + (diffTime) + "ms : " + markdownTime +  "ms : " + renderDelay + "ms");
-  }
-  
-  function redraw() {
-    var startTime = (new Date()).getTime();
-    
-    var genHTML = markdown.makeHtml(inputarea.val());
-    preproc.html(genHTML);
-    
-    markdownTime = ((new Date()).getTime() - startTime);
-    
-    var patch = outputel.quickdiff("patch", preproc, ["mathSpan", "mathSpanInline"]);
-    
-    diffTime = ((new Date()).getTime() - startTime) - markdownTime;
-    setRenderDelay();
-    
-    if (patch.type !== "identical") {
-      if (patch.replace.length > 0) {
-        $.each(patch.replace, function (i, el) {
-          if (el.innerHTML) {
-            size_images(el);
-            MathJax.Hub.Typeset(el, function() {
-              lastrendertime = ((new Date()).getTime() - startTime) - diffTime - markdownTime;
-              setRenderDelay();
-            });
-          } else {
-            lastrendertime = ((new Date()).getTime() - startTime) - diffTime - markdownTime;
-            setRenderDelay();
-          }
-        });
-      }
-    } else {
-      lastrendertime = ((new Date()).getTime() - startTime) - diffTime - markdownTime;
-    }
-  }
+  $(window).resize(function () {
+    wHeight = $(window).height();
+    wWidth = $(window).width();
+    reconstrain(wHeight, wWidth);
+  });
   
   $("textarea").keyup(function () {
     if (this.timer) {
@@ -104,6 +59,88 @@ $(document).ready(function () {
     this.timer = setTimeout(redraw,renderDelay);
   });
   
+  $("#edit-enable a").click(function () {
+    if (!loaded) {
+      $("textarea").val("Loading..");
+      $.getJSON("/" + pagename + ".json", function (data) {
+        $("textarea").val(data.text);
+        $("#savebutton").attr("disabled", false);
+        loaded = true;
+        redraw();
+      });
+    }
+    
+    if (previewing || !narrowscreen) {
+      savePagePosition();
+    }
+    if (editing) {
+      saveEditorState();
+    }
+    
+    setPreviewing(false);
+    setEditing(!editing);
+    return false;
+  });
+  
+  $("#preview-enable a").click(function () {
+    if (narrowscreen && editing) {
+      if (previewing) {
+        savePagePosition();
+        setPreviewing(false);
+        $("body").toggleClass("preview", false);
+        restoreEditorState();
+      } else {
+        saveEditorState();
+        setPreviewing(true);
+        $("body").toggleClass("preview", true);
+        restorePagePosition();
+      }
+    }
+    return false;
+  });
+  
+  $("#savebutton").click(function () {
+    $.post("/" + pagename + ".json", {text:$("textarea").val(), password: $("#password").val()}, function (ret) {
+      if (ret && ret.status == "success") {
+        Grumble.show({message:"Saved successfully.", title:"Saved", icon:"success"});
+      } else {
+        if (ret && ret.status == "failure") {
+          Grumble.show({message:ret.message, title:"Error", icon:"error"});
+        } else {
+          Grumble.show({message:"Unknown response from the server.", title:"Error", icon:"error"});
+        }
+      }
+    }, "json");
+  });
+  
+  function setRenderDelay(rendertime) {
+    if (rendertime > 200) {
+      renderDelay = 400;
+    } else if (rendertime > 10) {
+      renderDelay = 50;
+    }
+  }
+  
+  function redraw() {
+    var startTime = (new Date()).getTime();
+    preproc.html(markdown.makeHtml(inputarea.val()));
+    var patch = outputel.quickdiff("patch", preproc, ["mathSpan", "mathSpanInline"]);
+    
+    if (patch.type !== "identical" && patch.replace.length > 0) {
+      $.each(patch.replace, function (i, el) {
+        if (el.innerHTML) {
+          size_images(el);
+          MathJax.Hub.Typeset(el, function() {
+            setRenderDelay((new Date()).getTime() - startTime);
+          });
+        } else {  
+          setRenderDelay((new Date()).getTime() - startTime);
+        }
+      });
+    } else {
+      setRenderDelay((new Date()).getTime() - startTime);
+    }
+  }
   
   function savePagePosition() {
     preview_scrolltop = $("body").scrollTop();
@@ -132,20 +169,20 @@ $(document).ready(function () {
   }
   
   function reconstrain(height, width) {
-    if (editting) {
-      $("textarea").height(height - 90);
+    if (editing) {
+      inputarea.height(height - 90);
     }
     
     switched = false
     if (width < 1260 && !narrowscreen) {
-      if (editting) {
+      if (editing) {
         savePagePosition();
       }
       narrowscreen = true;
       setPreviewing(false);
       switched = true;
     } else if (width >= 1260 && narrowscreen) {
-      if (editting) {
+      if (editing) {
         saveEditorState();
       }
       narrowscreen = false;
@@ -156,20 +193,17 @@ $(document).ready(function () {
     $("body").toggleClass("widescreen", !narrowscreen);
     $("body").toggleClass("narrowscreen", narrowscreen);
     
-    if (editting && switched && width >= 1260) {
+    if (editing && switched && !narrowscreen) {
       restorePagePosition();
       restoreEditorState();
     }
   }
   
-  reconstrain();
-  
-  // $('pre code').each(function(i, e) {hljs.highlightBlock(e, '    ')});
-  
-  function editprocessing() {
-    $("body").toggleClass("readonly", !editting);
-    $("body").toggleClass("editting", editting);
-    if (editting) {
+  function setEditing(edit) {
+    editing = edit;
+    $("body").toggleClass("readonly", !editing);
+    $("body").toggleClass("editing", editing);
+    if (editing) {
       reconstrain(wHeight, wWidth);
       restoreEditorState();
       $("#edit-enable a").text("CANCEL");
@@ -179,70 +213,5 @@ $(document).ready(function () {
       $("#edit-enable a").text("EDIT");
     }
   }
-  
-  editprocessing();
-  
-  $("#edit-enable").click(function () {
-    if (!loaded) {
-      $("textarea").val("Loading..");
-      $.getJSON("/" + pagename + ".json", function (data) {
-        $("textarea").val(data.text);
-        $("#savebutton").attr("disabled", false);
-        loaded = true;
-        redraw();
-      });
-    }
-    if (previewing || !narrowscreen) {
-      savePagePosition();
-    }
-    if (editting) {
-      saveEditorState();
-    }
-    editting = !editting;
-    
-    setPreviewing(false);
-    editprocessing();
-    return false;
-  });
-  
-  $("#preview-enable").click(function () {
-    if (wWidth < 1260 && editting) {
-      if (previewing) {
-        savePagePosition();
-      } else {
-        saveEditorState();
-      }
-      
-      setPreviewing(!previewing);
-      $("body").toggleClass("preview", previewing);
-      
-      if (previewing) {
-        restorePagePosition();
-      } else {
-        restoreEditorState();
-      }
-    }
-    return false;
-  });
-  
-  $(window).resize(function () {
-    wHeight = $(window).height();
-    wWidth = $(window).width();
-    reconstrain(wHeight, wWidth);
-  });
-  
-  $("#savebutton").click(function () {
-    $.post("/" + pagename + ".json", {text:$("textarea").val(), password: $("#password").val()}, function (ret) {
-      if (ret && ret.status == "success") {
-        Grumble.show({message:"Saved successfully.", title:"Saved", icon:"success"});
-      } else {
-        if (ret && ret.status == "failure") {
-          Grumble.show({message:ret.message, title:"Error", icon:"error"});
-        } else {
-          Grumble.show({message:"Unknown response from the server.", title:"Error", icon:"error"});
-        }
-      }
-    }, "json");
-  });
   
 });
