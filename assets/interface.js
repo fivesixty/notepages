f57f83cdfb04d40837d1a3aea450c24a5eb3e702
c-spencer <cs560@cam.ac.uk>
2011-03-23 16:53:43 +0000

    Encapsulate Page, lint

--- assets/interface.js
+++ assets/interface.js
@@ -2,7 +2,7 @@ function size_images(context) {
   $("img", context).not(".MathJax_strut").each(function (i, obj) {
     obj = $(obj);
     if (obj.width() > 625) {
-      scale = 625 / obj.width();
+      var scale = 625 / obj.width();
       obj.width(obj.width() * scale);
     }
   });
@@ -18,7 +18,7 @@ $.fn.quickdiff("filter", "mathSpanInline",
   function (a, b) {
     var aHTML = $.trim($("script", a).html()), bHTML = $.trim($(b).html());
     return ("%%" + aHTML + "%%") !== bHTML;
-  } );
+  });
 
 $.fn.quickdiff("filter", "mathSpan",
   function (node) { return (node.nodeName === "SPAN"
@@ -26,111 +26,38 @@ $.fn.quickdiff("filter", "mathSpan",
   function (a, b) {
     var aHTML = $.trim($("script", a).html()), bHTML = $.trim($(b).html());
     return ("$$" + aHTML + "$$") !== bHTML;
-  } );
-
-$(document).ready(function () {
-  var loaded = editing,
-    previewing = false,
+  });
+  
+var Page = (function () {
+  var previewing = false,
     editor_scrolltop = 0,
     editor_position = {start: 0, end: 0},
     preview_scrolltop = 0,
     markdown = new Showdown.converter(),
     narrowscreen = false,
-    inputarea = $("textarea"),
+    inputarea,
     preproc = $("<div></div>"),
-    outputel = $("#output > div"),
+    outputel,
     renderDelay = 50;
-    
-  reconstrain();
-  setEditing(editing);
   
-  var wHeight = $(window).height();
-  var wWidth = $(window).width();
-  $(window).resize(function () {
-    wHeight = $(window).height();
-    wWidth = $(window).width();
-    reconstrain(wHeight, wWidth);
-  });
-  
-  $("textarea").keyup(function () {
-    if (this.timer) {
-      clearTimeout(this.timer);
-    }
-    this.timer = setTimeout(redraw,renderDelay);
-  });
-  
-  $("#edit-enable a").click(function () {
-    if (!loaded) {
-      $("textarea").val("Loading..");
-      $.getJSON("/" + pagename + ".json", function (data) {
-        $("textarea").val(data.text);
-        $("#savebutton").attr("disabled", false);
-        loaded = true;
-        redraw();
-      });
-    }
-    
-    if (previewing || !narrowscreen) {
-      savePagePosition();
-    }
-    if (editing) {
-      saveEditorState();
-    }
-    
-    setPreviewing(false);
-    setEditing(!editing);
-    return false;
-  });
-  
-  $("#preview-enable a").click(function () {
-    if (narrowscreen && editing) {
-      if (previewing) {
-        savePagePosition();
-        setPreviewing(false);
-        $("body").toggleClass("preview", false);
-        restoreEditorState();
-      } else {
-        saveEditorState();
-        setPreviewing(true);
-        $("body").toggleClass("preview", true);
-        restorePagePosition();
-      }
-    }
-    return false;
-  });
-  
-  $("#savebutton").click(function () {
-    $.post("/" + pagename + ".json", {text:$("textarea").val(), password: $("#password").val()}, function (ret) {
-      if (ret && ret.status == "success") {
-        Grumble.show({message:"Saved successfully.", title:"Saved", icon:"success"});
-      } else {
-        if (ret && ret.status == "failure") {
-          Grumble.show({message:ret.message, title:"Error", icon:"error"});
-        } else {
-          Grumble.show({message:"Unknown response from the server.", title:"Error", icon:"error"});
-        }
-      }
-    }, "json");
-  });
-  
-  function setRenderDelay(rendertime) {
+  var setRenderDelay = function (rendertime) {
     if (rendertime > 200) {
       renderDelay = 400;
     } else if (rendertime > 10) {
       renderDelay = 50;
     }
-  }
-  
-  function redraw() {
+  };
+
+  var redraw = function () {
     var startTime = (new Date()).getTime();
     preproc.html(markdown.makeHtml(inputarea.val()));
     var patch = outputel.quickdiff("patch", preproc, ["mathSpan", "mathSpanInline"]);
-    
+
     if (patch.type !== "identical" && patch.replace.length > 0) {
       $.each(patch.replace, function (i, el) {
         if (el.innerHTML) {
           size_images(el);
-          MathJax.Hub.Typeset(el, function() {
+          MathJax.Hub.Typeset(el, function () {
             setRenderDelay((new Date()).getTime() - startTime);
           });
         } else {  
@@ -140,25 +67,25 @@ $(document).ready(function () {
     } else {
       setRenderDelay((new Date()).getTime() - startTime);
     }
-  }
-  
-  function savePagePosition() {
+  };
+
+  var savePagePosition = function () {
     preview_scrolltop = $("body").scrollTop();
-  }
-  function restorePagePosition() {
+  };
+  var restorePagePosition = function () {
     $("body").scrollTop(preview_scrolltop);
-  }
-  
-  function saveEditorState() {
-    editor_scrolltop = $("textarea").scrollTop();
-    editor_position = $("textarea").getSelection();
-  }
-  function restoreEditorState() {
-    $("textarea").setSelection(editor_position.start, editor_position.end);
-    $("textarea").scrollTop(editor_scrolltop);
-  }
-  
-  function setPreviewing(toggle) {
+  };
+
+  var saveEditorState = function () {
+    editor_scrolltop = inputarea.scrollTop();
+    editor_position = inputarea.getSelection();
+  };
+  var restoreEditorState = function () {
+    inputarea.setSelection(editor_position.start, editor_position.end);
+    inputarea.scrollTop(editor_scrolltop);
+  };
+
+  var setPreviewing = function (toggle) {
     previewing = toggle;
     $("body").toggleClass("preview", previewing);
     if (previewing) {
@@ -166,14 +93,16 @@ $(document).ready(function () {
     } else {
       $("#preview-enable a").text("PREVIEW");
     }
-  }
-  
-  function reconstrain(height, width) {
+  };
+
+  var reconstrain = function () {
+    var height = $(window).height(), width = $(window).width();
+
     if (editing) {
       inputarea.height(height - 90);
     }
-    
-    switched = false
+
+    var switched = false;
     if (width < 1260 && !narrowscreen) {
       if (editing) {
         savePagePosition();
@@ -189,29 +118,131 @@ $(document).ready(function () {
       setPreviewing(true);
       switched = true;
     }
-    
+
     $("body").toggleClass("widescreen", !narrowscreen);
     $("body").toggleClass("narrowscreen", narrowscreen);
-    
+
     if (editing && switched && !narrowscreen) {
       restorePagePosition();
       restoreEditorState();
     }
-  }
-  
-  function setEditing(edit) {
+  };
+
+  var setEditing = function (edit) {
     editing = edit;
     $("body").toggleClass("readonly", !editing);
     $("body").toggleClass("editing", editing);
     if (editing) {
-      reconstrain(wHeight, wWidth);
+      reconstrain();
       restoreEditorState();
       $("#edit-enable a").text("CANCEL");
     } else {
-      $("#page").css({height:"auto"});
+      $("#page").css({height: "auto"});
       restorePagePosition();
       $("#edit-enable a").text("EDIT");
     }
-  }
+  };
+  
+  return {
+    editing: function () {
+      if (arguments.length === 0) {
+        return editing;
+      } else {
+        setEditing(arguments[0]);
+      }
+    },
+    preview: function () {
+      if (arguments.length === 0) {
+        return previewing;
+      } else {
+        setPreviewing(arguments[0]);
+      }
+    },
+    savePagePosition: savePagePosition,
+    restorePagePosition: restorePagePosition,
+    saveEditorState: saveEditorState,
+    restoreEditorState: restoreEditorState,
+    init : function (name, input, output) {
+      pagename = name;
+      inputarea = input;
+      outputel = output;
+    },
+    reconstrain: reconstrain,
+    pagename: function () {
+      return pagename;
+    },
+    narrowscreen: function () {
+      return narrowscreen;
+    },
+    redraw: redraw
+  };
+}());
+
+$(document).ready(function () {
+  var loaded = editing,
+    inputarea = $("textarea"),
+    outputel = $("#output > div");
+  
+  Page.init(pagename, inputarea, outputel);
+  Page.reconstrain();
+  Page.editing(editing);
+  
+  $(window).resize(Page.reconstrain);
+  
+  $("textarea").keyup(function () {
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
+    this.timer = setTimeout(Page.redraw, Page.renderDelay);
+  });
+  
+  $("#edit-enable a").click(function () {
+    if (!loaded) {
+      inputarea.val("Loading..");
+      $.getJSON("/" + Page.pagename() + ".json", function (data) {
+        inputarea.val(data.text);
+        loaded = true;
+        Page.redraw();
+      });
+    }
+    
+    if (Page.preview() || !Page.narrowscreen()) {
+      Page.savePagePosition();
+    }
+    if (Page.editing()) {
+      Page.saveEditorState();
+    }
+    
+    Page.preview(false);
+    Page.editing(!Page.editing());
+    return false;
+  });
+  
+  $("#preview-enable a").click(function () {
+    if (Page.preview()) {
+      Page.savePagePosition();
+      Page.preview(false);
+      Page.restoreEditorState();
+    } else {
+      Page.saveEditorState();
+      Page.preview(true);
+      Page.restorePagePosition();
+    }
+    return false;
+  });
+  
+  $("#savebutton").click(function () {
+    $.post("/" + Page.pagename() + ".json", {text: inputarea.val(), password: $("#password").val()}, function (ret) {
+      if (ret && ret.status === "success") {
+        Grumble.show({message: "Saved successfully.", title: "Saved", icon: "success"});
+      } else {
+        if (ret && ret.status === "failure") {
+          Grumble.show({message: ret.message, title: "Error", icon: "error"});
+        } else {
+          Grumble.show({message: "Unknown response from the server.", title: "Error", icon: "error"});
+        }
+      }
+    }, "json");
+  });
   
 });
