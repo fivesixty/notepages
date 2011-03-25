43d1f143562a207755a99e432837a466fd3d9ae2
c-spencer <cs560@cam.ac.uk>
2011-03-25 17:02:11 +0000

    Add a syntax help panel in edit mode

--- assets/interface.js
+++ assets/interface.js
@@ -240,6 +240,18 @@ $(document).ready(function () {
   
   $(window).resize(Page.reconstrain);
   
+  $('.slide-out-div').tabSlideOut({
+      tabHandle: '.handle',                              //class of the element that will be your tab
+      pathToTabImage: 'assets/sidehelp.png',          //path to the image for the tab *required*
+      imageHeight: '122px',                               //height of tab image *required*
+      imageWidth: '40px',                               //width of tab image *required*    
+      tabLocation: 'left',                               //side of screen where tab lives, top, right, bottom, or left
+      speed: 300,                                        //speed of animation
+      action: 'click',                                   //options: 'click' or 'hover', action to trigger animation
+      topPos: '100px',                                   //position from the top
+      fixedPosition: false                               //options: true makes it stick(fixed position) on scroll
+  });
+  
   // On keyup, clear any timer and instate a new one using the current
   // render relay.
   $("textarea").keyup(function () {
